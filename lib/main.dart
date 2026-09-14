779e5d0f6193240397e48f55e0a500ca1aa74378
Shubham Sharma <170sharmashubham@gmail.com>
2026-09-14 19:10:18 +0530

    setup firebase CLI and configure project options

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:exppence_tracker/firebase_options.dart';
 import 'package:exppence_tracker/provider/transaction_provider.dart';
 import 'package:exppence_tracker/screen/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     ChangeNotifierProvider(
       create: (context) => TransactionProvider(),
